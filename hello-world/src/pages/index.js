e183af1a66d620bb78e823ce23643178705f3af7
Gautam Bajaj <890900+d34th4ck3r@users.noreply.github.com>
2020-06-04 18:28:50 +0900

    basic GraphQL

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,13 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Header from "../components/header"
 
 export default function Home() {
   return (
   	<Layout>
-  	  <h1> Trying typography in Gatsby</h1>
-  	  <p> Checking the paragraph styling and how it is different from everything else </p>
+  	  <h1> Amazing Pandas Eating Things </h1>
+  	  <div>
+  	    <img
+  	      src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+  	      alt="Group of pandas eating bamboo"
+  	    />
+  	  </div>
   	</Layout>
   )
 }

a3df89752a2b9cc4907851ef87255f861d4ed850
Gautam Bajaj <890900+d34th4ck3r@users.noreply.github.com>
2021-02-04 02:10:45 +0900

    Improving grammar

--- src/components/personal-info.js
+++ src/components/personal-info.js
@@ -30,7 +30,7 @@ export default class PersonalInfo extends React.Component {
     super(props);
     this.state = {
       nodeTitle: "About Me",
-      nodeDescription: "I am a Trilingual(English, 日本語, हिंदी) Software Engineer/Architect, based out of Tokyo, who started writing code at the age of 13 and being writing it ever since. Currently I am working as a Senior Software Engineer(Data Engineering) for an American Company and advisor for various startups. The buzzwords for me(right now) are: Reinforcement Learning, Kubernetes, and Blockchain Protocols. I frequently write about these topics which I plan to start posting here soon. Beside all the computer science stuff, I also like discussing about Philosophy and Psychology. Feel free to send a message, if you want start a discussion on any of these topics."
+      nodeDescription: "I am a Trilingual(English, 日本語, हिंदी) Software Engineer/Architect, based out of Tokyo, who started writing code at the age of 13 and being writing it ever since. Currently, I am working as a Senior Software Engineer(Data Engineering) for an American Company and advisor for various startups. The buzzwords for me(right now) are Reinforcement Learning, Kubernetes, and Blockchain Protocols. I frequently write about these topics which I plan to start posting here soon. Besides computer-science related stuff, I also like discussing philosophy and psychology. Feel free to send a message, if you want to start a discussion on any of these topics."
     }
   }
 
@@ -57,7 +57,7 @@ export default class PersonalInfo extends React.Component {
               About Me
             </div>
             <div hidden={true} id="about-me-desc">
-              I am a Trilingual(English, 日本語, हिंदी) Software Engineer/Architect, based out of Tokyo, who started writing code at the age of 13 and being writing it ever since. Currently I am working as a Senior Software Engineer(Data Engineering) for an American Company and advisor for various startups. The buzzwords for me(right now) are: Reinforcement Learning, Kubernetes, and Blockchain Protocols. I frequently write about these topics which I plan to start posting here soon. Beside all the computer science stuff, I also like discussing about Philosophy and Psychology. Feel free to send a message, if you want start a discussion on any of these topics.
+              I am a Trilingual(English, 日本語, हिंदी) Software Engineer/Architect, based out of Tokyo, who started writing code at the age of 13 and being writing it ever since. Currently, I am working as a Senior Software Engineer(Data Engineering) for an American Company and advisor for various startups. The buzzwords for me(right now) are Reinforcement Learning, Kubernetes, and Blockchain Protocols. I frequently write about these topics which I plan to start posting here soon. Besides computer-science related stuff, I also like discussing philosophy and psychology. Feel free to send a message, if you want to start a discussion on any of these topics.
             </div>
           </div>
           <div className="node-btn button d-flex align-items-center" role="button" tabIndex={-1} id="work-ex" onClick={(e) => this.setCurrentNode(e)} onKeyPress={this.handleKeyPress}>
@@ -65,8 +65,8 @@ export default class PersonalInfo extends React.Component {
               Work Experience
             </div>
             <div hidden={true} id="work-ex-desc">
-              Continents I have worked in: North America, Europe, Asia. <br/>
-              Companies I have worked for: SoftBank, Google, Square Enix, Teradata, Wikia. <br/>
+              Continents I have worked in are North America, Europe, Asia. <br/>
+              Companies I have worked for include SoftBank, Google, Square Enix, Teradata, Wikia, etc. <br/>
             </div>
           </div>
           <div className="node-btn button d-flex align-items-center" role="button" tabIndex={-1} id="open-src" onClick={(e) => this.setCurrentNode(e)} onKeyPress={this.handleKeyPress}>
